362f7bbd367c461cee75c5d5c9525f477d7291ce
Axel Michel <axelmichel44@gmail.com>
2026-08-24 18:47:44 +0200

    update

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import "./collapse.js";
 import "./barba.js";
 
 const BUILD_VERSION = new Date().toISOString().slice(0, 10);
-console.log(`%c[Overflo3] main.js — build ${BUILD_VERSION}`, "color:#7dd3fc");
+console.log(`%c[Overflo] main.js — build v2.0.2 ${BUILD_VERSION}`, "color:#7dd3fc");
